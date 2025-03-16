cba86126ce5086ca944fefd15242e5e38947f6d4
wuwenbang <wuwenbang@foxmail.com>
2025-03-16 15:43:46 +0800

    fix: text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About Me</h1>
       <p className="mb-4">我是 Leo，一名程序员，这是我的博客集。</p>
     </section>
   );
