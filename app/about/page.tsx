8dcacb3562e2aa86a29d3ed26f2894ca168a9a8c
wuwenbang <wuwenbang@foxmail.com>
2025-03-16 14:00:30 +0800

    feat: init project

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "About",
+  description: "About me.",
+};
+
+export default function Page() {
+  return (
+    <section>
+      <p className="mb-4">我是 Leo，一名程序员，这是我的博客集。</p>
+    </section>
+  );
+}
