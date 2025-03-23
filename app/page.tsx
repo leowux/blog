a94400bc4fa6bf464c90a5fcf1adbe6bb6cab6d3
wuwenbang <wuwenbang@foxmail.com>
2025-03-23 18:09:10 +0800

    chore: add prettierrc

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import { TagCloud } from "./components/tag-cloud";
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Leo's Blog
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Leo's Blog</h1>
       <p className="mb-4">我是 Leo，一名程序员，这是我的博客集。</p>
       <div className="my-8">
         <BlogPosts />
