2882c4fc3455eb514129edeb9e490e49b9f782b8
leowux <wuwenbang@foxmail.com>
2025-05-25 16:31:00 +0800

    style: home page layout

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,14 +21,14 @@ export function BlogPosts() {
               className="flex flex-col space-y-1 mb-2"
               href={`/blog/${post.slug}`}
             >
-              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                <p className="text-neutral-600 w-[150px] tabular-nums">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </p>
-                <p className="text-neutral-900 tracking-tight">
+              <li className="w-full flex flex-col md:flex-row md:space-x-2 justify-between">
+                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                   {post.metadata.title}
                 </p>
-              </div>
+                <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
+                  {formatDate(post.metadata.publishedAt)}
+                </p>
+              </li>
             </Link>
           </div>
         ))}
