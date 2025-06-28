fe0710f98a17d01a86d7fb64e16af8611c11eabe
leowux <wuwenbang@foxmail.com>
2025-06-28 11:20:34 +0800

    fix: centered

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,22 +38,11 @@ export const metadata: Metadata = {
 
 const cx = (...classes) => classes.filter(Boolean).join(" ");
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={cx(
-        "text-black bg-white",
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className="antialiased max-w-xl mx-auto mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+    <html lang="en" className={cx("w-full min-h-screen text-black bg-white", GeistSans.variable, GeistMono.variable)}>
+      <body className="w-full min-h-screen antialiased">
+        <main className="max-w-xl mx-auto flex-auto min-w-0 mt-8 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
           <Footer />
